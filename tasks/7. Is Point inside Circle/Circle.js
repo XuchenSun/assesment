7ff9d5f88639288bb5ac5b2c35029a4a8e5fd2df
sunxu <xuchens@mun.ca>
2021-08-24 13:23:17 -0230

    16    passing

--- tasks/7. Is Point inside Circle/Circle.js
+++ tasks/7. Is Point inside Circle/Circle.js
@@ -11,7 +11,14 @@ export default function(center, radius) {
       return radius ** 2 * Math.PI;
     },
     includes(Point) {
-      // TODO: write your code here
+      let distance=0;// let is better than var because it use less memory
+      distance=(Point.x-center.x)*(Point.x-center.x)+(Point.y-center.y)*(Point.y-center.y);//get the distance*distance
+      distance=Math.sqrt(distance);//get the distance
+      return distance <= radius;// method one
+      // method two
+      // if(distance>radius) return false
+      // else return true
+      // Comments method one is more quick when the program is running but method two is easier to understand
     }
   }
 }
