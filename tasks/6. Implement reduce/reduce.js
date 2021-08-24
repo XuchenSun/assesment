2fd4072e199be93d4796441e902b050c8a0e3f50
sunxu <xuchens@mun.ca>
2021-08-24 13:07:40 -0230

    14 passing

--- tasks/6. Implement reduce/reduce.js
+++ tasks/6. Implement reduce/reduce.js
@@ -1,3 +1,4 @@
 export function Reduce(array, reducer, initialValue) {
-  // TODO: write your code here
+  if (initialValue===0){ return array.reduce(reducer)}// if array has number, return the summary
+  else return array.join("");// if array is string, return a long string.
 }
