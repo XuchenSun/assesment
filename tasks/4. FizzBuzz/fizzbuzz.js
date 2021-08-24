2803389ce3e4c2ac13567318a56e428c98dfb9c7
sunxu <xuchens@mun.ca>
2021-08-24 12:55:45 -0230

    14 passing

--- tasks/4. FizzBuzz/fizzbuzz.js
+++ tasks/4. FizzBuzz/fizzbuzz.js
@@ -1,3 +1,10 @@
 export function getFizzBuzzUntil(n) {
-  // TODO: write your code here
+   var array=[];
+   for(let i=1;i<=n;i++){// use let command because it use less memory ES6
+       if((i%3)===0&&(i%5)===0){array.push('FizzBuzz')}// this command must be the first to output the FizzBuzz in conditon of 3 and 5 times
+       else if((i%3)===0){array.push('Fizz')}// return Fizz if it is 3 times
+       else if((i%5)===0){array.push('Buzz')}//return Buzz if it is 5 times
+       else {array.push(i.toString())}//return i
+   }
+   return array//return array of FizzBuzz
 }
--- tasks/5. Caesar cipher/caesar.js
+++ tasks/5. Caesar cipher/caesar.js
@@ -1,3 +1,17 @@
 export function encryptCaesar(inputString, key) {
-  // TODO: write your code here
+    var array = inputString.toUpperCase().split('');
+    var string_after_encoder= [];
+    for(let i=0;i<array.length;i++){
+        let array_has_offset = array[i].split('');
+        for(let j=0;j<array_has_offset.length;j++){
+            let num = array_has_offset[j].charCodeAt();
+            if(num >=65 && num <=90){
+                array_has_offset[j] = num+key>90?String.fromCharCode(64+(num+key-90)):String.fromCharCode(num+key);// use key to define the offset
+            }
+        }
+        string_after_encoder.push(array_has_offset.join(''));
+    }
+    return string_after_encoder.join('');
+
+
 }
